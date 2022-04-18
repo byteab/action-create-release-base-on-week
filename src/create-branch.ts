2bf157a21fcfa90a66058c9956bfdfd18fc8b62b
Ehsan sarshar <ehsan.herai@gmail.com>
2022-04-18 09:56:16 +0430

    business code implemented

--- src/create-branch.ts
+++ src/create-branch.ts
@@ -1,28 +1,49 @@
 import { Context } from '@actions/github/lib/context';
+import { getOctokit, context } from '@actions/github';
+import dayjs from 'dayjs';
 
-export async function createBranch(getOctokit: any, context: Context, branch: string, sha?: string) {
-  const toolkit = getOctokit(githubToken());
-  // Sometimes branch might come in with refs/heads already
-  branch = branch.replace('refs/heads/', '');
-  const ref = `refs/heads/${branch}`;
+// get current date
+// compare it with a specific date
+// calcualte the branch version
+// check if branch already exists if no create it
+export async function createBranch() {
+ 
+  let epochDate = dayjs('2022-04-07').hour(7).minute(30);
+  let todayDate = dayjs();
 
-  // throws HttpError if branch already exists.
-  try {
-    await toolkit.rest.repos.getBranch({
-      ...context.repo,
-      branch,
-    });
-  } catch (error: any) {
-    if (error.name === 'HttpError' && error.status === 404) {
-      const resp = await toolkit.rest.git.createRef({
-        ref,
-        sha: sha || context.sha,
+  let hours = todayDate.diff(epochDate, 'hours');
+  const days = Math.floor(hours / 24);
+
+  // every two week
+  // 17 is the last release number
+  let count = Math.floor(days / 14) + 17;
+
+  // create new branch
+  if(count) {
+
+    const toolkit = getOctokit(githubToken());
+    // Sometimes branch might come in with refs/heads already
+    let branch = `branch-${count}`;
+    const ref = `refs/heads/${branch}`;
+
+    // throws HttpError if branch already exists.
+    try {
+      await toolkit.rest.repos.getBranch({
         ...context.repo,
+        branch,
       });
+    } catch (error: any) {
+      if (error.name === 'HttpError' && error.status === 404) {
+        const resp = await toolkit.rest.git.createRef({
+          ref,
+          sha: context.sha,
+          ...context.repo,
+        });
 
-      return resp?.data?.ref === ref;
-    } else {
-      throw Error(error);
+        return resp?.data?.ref === ref;
+      } else {
+        throw Error(error);
+      }
     }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,7 @@ import { createBranch } from './create-branch';
 
 async function run() {
   try {
-    const branch = core.getInput('branch');
-    const sha = core.getInput('sha');
-    core.debug(`Creating branch ${branch}`);
-    const isCreated = await createBranch(getOctokit, context, branch, sha);
+    const isCreated = await createBranch();
     core.setOutput('created', Boolean(isCreated));
   } catch (error: any) {
     core.setFailed(error.message);
